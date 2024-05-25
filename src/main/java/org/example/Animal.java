a4ff2860bdcdc02449c1d2884998b1de9a7545b5
Cristian Maya G <cmaya@cesde.net>
2024-05-25 15:11:58 -0500

    Solucion taller 25 mayo

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -2,24 +2,53 @@ package org.example;
 
 public class Animal {
 
-    String nombre;
-    String genero
-    double peso=true;
-    Animal pareja=new;
+    private String nombre;
+    private String genero;
+    private Double peso;
+    private Animal pareja;
 
 
-    public Animal(String nombre, double peso) {
+    public Animal(String nombre, Double peso) {
         this.nombre = nombre;
         this.peso = peso;
+    }
+
+    public Animal() {
     }
 
     void encontrarPareja(Animal pareja){
         this.pareja=pareja;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getGenero() {
+        return genero;
+    }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
+    public Double getPeso() {
+        return peso;
+    }
 
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Animal getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(Animal pareja) {
+        this.pareja = pareja;
+    }
 }
---
+++ src/main/java/org/example/readme.md
@@ -0,0 +1,29 @@
+¿Cuántas clases tiene el proyecto?
+Tiene 3 clases
+-
+¿En cuál clase se define el programa principal?
+En la clase Zoologico
+-
+¿Cuántos objetos de la clase Animal se crean en la clase principal?
+Se crean 2 objetos
+-
+¿Cuántos y cuáles son los atributos de la clase Animal?
+Son 4 atributos: nombre, genero, edad y pareja
+-
+¿Cuáles atributos de la clase Animal no son primitivos?
+El atributo pareja
+-
+¿Qué pasa si eliminamos la línea 17 de la clase Zoologico? ?
+El ciclo if lo lleva a "Aun no puedes tener hijos"
+
+¿Cuántos métodos tiene la clase Familia
+Tiene 4 métodos
+-
+¿Cuántos parámetros tiene el método tenerHijo() de la clase Familia?
+Tiene 1 parámetro
+-
+¿Cuántos atributos inicializa el constructor de la clase Familia?
+Inicializa 2 atributos
+-
+¿Qué tipo de retorno tiene el método tenerHijo()?
+Retorna el objeto hijo de la clase Animal
