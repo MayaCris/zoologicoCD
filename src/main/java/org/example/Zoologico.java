e443253c64555bf5a92b64e8c26287bf5074be8d
Cristian Maya G <cmaya@cesde.net>
2024-06-01 14:53:46 -0500

    Fix names

--- src/main/java/org/example/Zoologico.java
+++ src/main/java/org/example/Zoologico.java
@@ -5,12 +5,12 @@ public class Zoologico {
     public static void main(String[] args) {
 
         Animal animal1 = new Animal();
-        animal1.setNombre("Cebra");
+        animal1.setNombre("Caballo");
         animal1.setGenero("F");
 
 
         Animal animal2 = new Animal();
-        animal2.setNombre("Caballo");
+        animal2.setNombre("Cebra");
         animal2.setGenero("M");
 
 
